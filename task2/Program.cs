10df14cbc1d3a8784c657e54340a8802f460e922
Daria <darya.shumilova.03@bk.ru>
2023-01-30 19:29:53 +0600

    first commit

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = a%10;
+if (a>99)
+{
+     Console.WriteLine(result);
+}
+else
+{
+     Console.WriteLine("Третьей цифры нет ");
+}
